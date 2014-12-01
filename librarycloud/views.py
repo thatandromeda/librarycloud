d9b459e75577e18d88cec33a47b0ae4bf252614c
Andromeda Yelton <andromeda.yelton@gmail.com>
2014-12-01 11:43:12 -0500

    now i can query the API and get results in a way which is useful

--- librarycloud/views.py
+++ librarycloud/views.py
@@ -3,13 +3,15 @@ from django.views.generic import TemplateView
 
 import requests
 
+from search import LibraryCloud
+
 class StackView(TemplateView):
     template_name = 'stackview.html'
 
     def get_context_data(self, **kwargs):
         context = super(StackView, self).get_context_data(**kwargs)
-        url = 'http://api.lib.harvard.edu/v2/items.json?subject.topic=women&sort.desc=stackscore'
-        response = requests.get(url).json()
-        context['response'] = response
+        lc = LibraryCloud()
+        results = lc.search('women')
+        context['results'] = results.items
         return context
 
