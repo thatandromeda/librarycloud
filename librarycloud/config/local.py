6b8bfbcc45c57634b823a493ad65413eb18974d7
Andromeda Yelton <andromeda.yelton@gmail.com>
2014-12-01 15:44:32 -0500

    debug toolbar is bad for demos

--- librarycloud/config/local.py
+++ librarycloud/config/local.py
@@ -28,17 +28,17 @@ class Local(Common):
     # End mail settings
 
     # django-debug-toolbar
-    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-    INSTALLED_APPS += ('debug_toolbar',)
+    #MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    #INSTALLED_APPS += ('debug_toolbar',)
 
-    INTERNAL_IPS = ('127.0.0.1',)
+    #INTERNAL_IPS = ('127.0.0.1',)
 
-    DEBUG_TOOLBAR_CONFIG = {
-        'DISABLE_PANELS': [
-            'debug_toolbar.panels.redirects.RedirectsPanel',
-        ],
-        'SHOW_TEMPLATE_CONTEXT': True,
-    }
+    #DEBUG_TOOLBAR_CONFIG = {
+    #    'DISABLE_PANELS': [
+    #        'debug_toolbar.panels.redirects.RedirectsPanel',
+    #    ],
+    #    'SHOW_TEMPLATE_CONTEXT': True,
+    #}
     # end django-debug-toolbar
 
     # Your local stuff: Below this line define 3rd party libary settings
