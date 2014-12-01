c4976e27d9936075e6662e200d8ce15913d7fa9e
Andromeda Yelton <andromeda.yelton@gmail.com>
2014-12-01 13:39:46 -0500

    woo yeah about page

--- librarycloud/urls.py
+++ librarycloud/urls.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-#from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,9 +13,9 @@ admin.autodiscover()
 from views import StackView
 
 urlpatterns = patterns('',
-    #url(r'^about/$',
-    #    TemplateView.as_view(template_name='pages/about.html'),
-    #    name="about"),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='pages/about.html'),
+        name="about"),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
